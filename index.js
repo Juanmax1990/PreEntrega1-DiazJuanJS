2e7ab625bc3abb1f1aeca2bdd946233d249ef707
Juanma <diazjuanma2505@gmail.com>
2023-03-04 18:44:11 -0300

    Ultimo

--- index.js
+++ index.js
@@ -2,39 +2,73 @@ let nombreyapellido = prompt('ingresa tu nombre y apellido');
 console.log(nombreyapellido);
 alert('Bienvenido a Compumax '+nombreyapellido);
 let edad = parseInt(prompt('Ingresa tu edad'));
-console.log(edad)
+console.log(edad);
 const email = prompt('Ingresa tu email');
 console.log(email);
 
-const accion = prompt('¿Que desea comprar? \n Placa Madre  \n Microprocesadores');
+let respuesta = false;
+let precioPlaca = 0; 
+let precioMicro = 0; 
 
-if (accion === 'Placa Madre'){
-    let compra=prompt('INTEL \n AMD');
-    switch(compra){
+while (!respuesta) {
+  const accion = prompt('¿Qué desea comprar? \n Placa Madre \n Microprocesadores');
+  switch (accion) {
+    case 'Placa Madre':
+      let compraPlaca = prompt('¿Quieres comprar una placa INTEL o AMD?');
+      switch (compraPlaca) {
         case 'INTEL':
-            alert('Solo contamos con stcok de ASUS ROG Z370-H');
-            break;
-
+          alert('Solo contamos con stock de ASUS ROG Z370-H');
+          precioPlaca = calcularPrecio('INTEL', 'Placa Madre'); // Asignar valor a precioPlaca
+          alert('El precio de la placa madre es de $' + precioPlaca);
+          break;
         case 'AMD':
-            alert('Contamos con marcas comom MSI, ASUS Y GIGABYTE');
-            break
-                       
-    }
+          alert('Contamos con marcas como MSI, ASUS y GIGABYTE');
+          precioPlaca = calcularPrecio('AMD', 'Placa Madre'); // Asignar valor a precioPlaca
+          alert('El precio de la placa madre es de $' + precioPlaca)
+          break;
+        default:
+          alert('Por favor, ingrese una opción válida');
+          break;
+      }
+      console.log(compraPlaca);
+      respuesta = true;
+      break;
+      ;
+
+    case 'Microprocesadores':
+      let compraMicro = prompt('¿Quieres comprar un micro INTEL o AMD?');
+      switch (compraMicro) {
+        case 'INTEL':
+          alert('Contamos con todos los modelos de la gama "i"');
+          precioMicro = calcularPrecio('INTEL', 'Microprocesadores'); // Asignar valor a precioPlaca
+          alert('El precio de la placa madre es de $' + precioMicro);
+          break;
+        case 'AMD':
+          alert('Tenemos los modelos Ryzen y para lo más exigentes, los Threadripper');
+          precioMicro = calcularPrecio('AMD', 'Microprocesadores'); // Asignar valor a precioPlaca
+          alert('El precio de la placa madre es de $' + precioMicro);
+          break;
+        default:
+          alert('Por favor, ingrese una opción válida');
+          break;
+      }
+      console.log(compraMicro);
+      respuesta = true;
+
+  }
 }
 
-if (accion === 'Microprocesadores'){
-    let compra=prompt('INTEL \n AMD');
-    switch(compra){
-        case 'INTEL':
-        alert('Contamos con todos los modelos de la gama "i"')
-        break
+function calcularPrecio(opcion1, opcion2) {
+  let precio = 0;
+  if (opcion1 === 'INTEL' && opcion2 === 'Placa Madre') {
+    precio = 200;
+  } else if (opcion1 === 'AMD' && opcion2 === 'Placa Madre') {
+    precio = 150;
+  } else if (opcion1 === 'INTEL' && opcion2 === 'Microprocesadores') {
+    precio = 300;
+  } else if (opcion1 === 'AMD' && opcion2 === 'Microprocesadores'){
+    precio = 250;
 
-        case 'AMD':
-        alert('Tenemos los modelos Ryzen y para lo mas exigentes, los Threadripper');
-        break
-
-    }
+  }
+return precio
 }
-
-
-
